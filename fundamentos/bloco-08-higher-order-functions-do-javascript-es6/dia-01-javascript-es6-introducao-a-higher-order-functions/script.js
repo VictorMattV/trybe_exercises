35194b54af26c21cfbb664a477af6fc18407917e
Victor <matheus0588@outlook.com>
2022-05-16 12:37:24 -0300

    Feito exercícios do dia 8.1

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,54 @@
+//1
+
+const employeeGenerator = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return { fullName, email: `${email}@trybe.com` };
+  };
+  
+  const newEmployees = (employeeGenerator) => {
+    const employees = {
+      id1: employeeGenerator('Pedro Guerra'),
+      id2: employeeGenerator('Luiza Drumond'),
+      id3: employeeGenerator('Carla Paiva'),
+    };
+    return employees;
+  };
+  
+  console.log(newEmployees(employeeGenerator));
+
+  //2
+
+  const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
+
+//3
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const compareAnswers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+const countPoints = (rightAnswers, studentAnswers, action) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    contador += actionReturn;
+  }
+  return `Resultado final: ${contador} pontos`;
+};
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+
