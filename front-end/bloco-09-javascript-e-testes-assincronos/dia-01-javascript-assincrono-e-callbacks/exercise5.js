c1789b0a5a55bf0a77bf77f507518cbb4de1028a
Victor <matheus0588@outlook.com>
2022-06-01 12:25:06 -0300

    Feito exercícios do dia 9.1

---
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercise5.js
@@ -0,0 +1,22 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+    const maxTemperature = 58;
+    return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9 / 5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+    console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+    console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const sendMarsTemperature = (callback) => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => callback(currentTemperature), messageDelay());
+};
+
+sendMarsTemperature(temperatureInFahrenheit);
+sendMarsTemperature(greet);
