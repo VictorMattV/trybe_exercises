79b4a3da1b3fb8658c86ee4245de7387e65a3dac
Victor <matheus0588@outlook.com>
2022-06-02 12:32:37 -0300

    Feito exercícios do dia 9.2

---
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -0,0 +1,28 @@
+const fetchCoins = async () => {
+    const url = 'https://api.coincap.io/v2/assets';
+
+    const coins = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.data)
+        .catch((error) => error.toString());
+
+    return coins;
+}
+
+const setCoins = async () => {
+    const coins = await fetchCoins();
+
+    const coinsList = document.getElementById('coins-list');
+
+    coins
+        .filter((coin) => Number(coin.rank) <= 10)
+        .forEach((coin) => {
+            const newLi = document.createElement('li');
+
+            newLi.innerText = `${coin.name} (${coin.symbol}): ${coin.priceUsd}`;
+
+            coinsList.appendChild(newLi);
+        });
+}
+
+window.onload = () => setCoins();
