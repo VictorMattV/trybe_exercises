34ee4d351300c3f1fc3e89ad54db3f5801c1599e
Victor <matheus0588@outlook.com>
2022-06-21 10:23:42 -0300

    Feito exercícios do dia 10.2

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokedex.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokedex.js
@@ -4,11 +4,6 @@ import Pokemon from './Pokemon';
 class Pokedex extends React.Component {
     render() {
         const { pokemons } = this.props;
-        // Recebemos do App.js, através da props "pokemons", o nosso array de pokemons.
-        // Devemos fazer um map, e nele renderizar o componente <Pokemon>, que receberá
-        // por props cada item do array.
-        // Lembre-se: é no componente Pokemon que iremos
-        // renderizar todas as informações necessárias para a exibição.
         return (
             <div className="pokedex">
                 {pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
@@ -4,13 +4,6 @@ import PropTypes from 'prop-types';
 class Pokemon extends React.Component {
     render() {
         const { pokemon: { name, type, averageWeight, image } } = this.props;
-        // Fizemos um map no nosso array de pokemons
-        // que vai renderizar o componente Pokemon para cada item do array.
-        // Então, recebemos a props "pokemon" que é um objeto do array de pokemons
-        // Nesses objetos temos informações como name, type e etc, então
-        // desestruturamos essas informações e renderizamos em uma tag HTML,
-        // no caso, utilizamos um <p>.
-
         return (
             <div className="pokemon">
                 <div>
