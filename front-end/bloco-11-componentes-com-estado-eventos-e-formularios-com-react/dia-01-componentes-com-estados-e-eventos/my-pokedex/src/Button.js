c7b031480220183924420bc2ca02cf1d010a72fd
Victor <matheus0588@outlook.com>
2022-06-23 12:26:04 -0300

    Feito exercícios do dia 11.1

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estados-e-eventos/my-pokedex/src/Button.js
@@ -0,0 +1,27 @@
+import { bool, func, node, string } from 'prop-types';
+import React from 'react';
+import './button.css';
+
+const Button = ({ className, children, disabled, onClick }) => (
+  <button
+    onClick={ onClick }
+    className={ `button-text ${className}` }
+    disabled={ disabled }
+    type="button"
+  >
+    { children }
+  </button>
+);
+
+Button.propTypes = {
+  className: string.isRequired,
+  children: node.isRequired,
+  disabled: bool,
+  onClick: func.isRequired,
+};
+
+Button.defaultProps = {
+  disabled: false,
+};
+
+export default Button;
