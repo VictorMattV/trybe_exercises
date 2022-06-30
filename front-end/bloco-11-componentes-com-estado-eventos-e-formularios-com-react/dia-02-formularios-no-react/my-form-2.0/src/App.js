ff0620cc1b783c27f6789107702fb0d85e0c3d70
Victor <matheus0588@outlook.com>
2022-06-30 11:32:53 -0300

    Feito exercícios do dia 12.1

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/my-form-2.0/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Components/Form';
+import './styles/global.css'
+
+function App() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default App;
